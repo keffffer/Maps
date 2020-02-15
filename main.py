89ff815ab4133dd972662a6ade0662f2952ba41a
keffffer <kefer04maya@gmail.com>
2020-02-15 18:05:30 +0700

    Добавьте обработку клавиш PgUp и PgDown, по нажатию на которые необходимо увеличивать и уменьшать масштаб отображения карты.
    Требуется также отслежвать предельные значения, за которые значения переменных не должны заходить.

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import requests
 import pygame
 from io import BytesIO
 from PIL import Image
+
 pygame.init()
 size = width, height = 600, 450
 api_server = "http://static-maps.yandex.ru/1.x/"
 
 lon = "35"
 lat = "45"
-delta = "0.5"
+z = 2
 
 params = {
     "ll": ",".join([lon, lat]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
+    "l": "map",
+    "size": "600,450",
+    "z": str(z)
 }
 response = requests.get(api_server, params=params)
 Image.open(BytesIO(response.content)).save("saved.png")
@@ -24,6 +26,21 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEDOWN:
+                if z > 0:
+                    z -= 1
+            if event.key == pygame.K_PAGEUP:
+                if z < 17:
+                    z += 1
+            params = {
+                "ll": ",".join([lon, lat]),
+                "l": "map",
+                "size": "600,450",
+                "z": str(z)
+            }
+            response = requests.get(api_server, params=params)
+            Image.open(BytesIO(response.content)).save("saved.png")
     image = pygame.image.load("saved.png")
     screen.blit(image, (0, 0))
     pygame.display.flip()
